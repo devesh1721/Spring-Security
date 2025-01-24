331a83044de4fc86a3b25b77e9e956c83dfa3d83
Devesh Sharma <deveshsharma@bdo.in>
2025-01-24 13:08:41 +0530

    Adding the user and password in application.properties

--- src/main/java/com/devesh/security/config/SpringSecurityConfig.java
+++ src/main/java/com/devesh/security/config/SpringSecurityConfig.java
@@ -2,24 +2,32 @@ package com.devesh.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.servlet.configuration.WebMvcSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig   {
+public class SpringSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->auth
-                .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                .requestMatchers("/api/user/**").hasRole("USER")
-                .anyRequest().authenticated());
-        return httpSecurity.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/user/**").hasRole("USER")
+                        .anyRequest().authenticated())
+                //This is for the form login in the screen
+                .formLogin(Customizer.withDefaults())
+                //This is for the postman for enable login
+                .httpBasic(Customizer.withDefaults())
+                //This is used to manage the stateless means for every request you need to share the username and password
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
--- src/main/java/com/devesh/security/controller/UserController.java
+++ src/main/java/com/devesh/security/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.devesh.security.controller;
 
 import com.devesh.security.model.DeveshUser;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    public void fun(){
-
-        DeveshUser deveshUser = new DeveshUser();
-        deveshUser.setUserId("1");
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "User Fetch SuccessFully";
     }
 }
